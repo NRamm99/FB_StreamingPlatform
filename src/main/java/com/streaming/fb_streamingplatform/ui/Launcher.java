8edd0a22563137f454695f2b7aa351a78a9279dc
NRamm99 <162293775+NRamm99@users.noreply.github.com>
2026-02-03 21:35:57 +0100

    Add favorite removal support
    Add a remove method to FavoriteRepository to delete a favorite by user and movie IDs. Implement StreamingService.removeFavorite with validations for user and movie existence, checks that the movie is currently a favorite, and deletion logic (throws descriptive exceptions if checks fail). Also include minor whitespace/formatting cleanup in Launcher.main.

--- src/main/java/com/streaming/fb_streamingplatform/ui/Launcher.java
+++ src/main/java/com/streaming/fb_streamingplatform/ui/Launcher.java
@@ -5,8 +5,7 @@ import javafx.application.Application;
 
 public class Launcher {
 
-    public static void main(String[] args) {
-        Application.launch(MainApp.class, args);
-
+    public static void main(String[] args)  {
+       Application.launch(MainApp.class, args);
     }
 }
