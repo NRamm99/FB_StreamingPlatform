414ca2f6e3b32e22a490056cd903dda53b77fb65
Havreberg <oliverz@live.dk>
2026-02-03 20:51:58 +0100

    Add validation in service layer

--- src/main/java/com/streaming/fb_streamingplatform/service/StreamingService.java
+++ src/main/java/com/streaming/fb_streamingplatform/service/StreamingService.java
@@ -1,22 +1,27 @@
 package com.streaming.fb_streamingplatform.service;
 
 import com.streaming.fb_streamingplatform.infrastructure.DatabaseConfig;
+import com.streaming.fb_streamingplatform.model.Movie;
 import com.streaming.fb_streamingplatform.model.User;
 import com.streaming.fb_streamingplatform.repository.FavoriteRepository;
+import com.streaming.fb_streamingplatform.repository.MovieRepository;
 import com.streaming.fb_streamingplatform.repository.UserRepository;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class StreamingService {
 
     private final UserRepository userRepository;
     private final FavoriteRepository favoriteRepository;
+    private final MovieRepository movieRepository;
 
     public StreamingService() {
         DatabaseConfig config = new DatabaseConfig();
         this.userRepository = new UserRepository(config);
         this.favoriteRepository = new FavoriteRepository(config);
+        this.movieRepository = new MovieRepository(config);
     }
 
     public Optional<User> findUserByEmail(String email) {
@@ -31,7 +36,25 @@ public class StreamingService {
         }
     }
 
-    public void addFavorite(int userId, int movieId) throws SQLException {
-       favoriteRepository.add(userId, movieId);
+    public void addFavorite(int userId, int movieId) throws Exception {
+        // Check if user exists
+        if (userRepository.getByUserId(userId).isEmpty()) {
+            throw new Exception("User with that ID does not exist");
+        }
+
+        // Check if movie exists
+        if (movieRepository.getByMovieId(movieId).isEmpty()) {
+            throw new Exception("Movie with that ID does not exist");
+        }
+
+        // Check if user already have that favorite movie
+        List<Movie> favMovies = favoriteRepository.getFavoritesByUserId(userId);
+
+            for(Movie mov : favMovies) {
+                if (mov.getId() == movieId) {
+                    throw new Exception("Movie is already favorite");
+                }
+            }
+        favoriteRepository.add(userId, movieId);
     }
 }
--- src/main/java/com/streaming/fb_streamingplatform/ui/Launcher.java
+++ src/main/java/com/streaming/fb_streamingplatform/ui/Launcher.java
@@ -1,17 +1,12 @@
 package com.streaming.fb_streamingplatform.ui;
 
 
-import com.streaming.fb_streamingplatform.repository.FavoriteRepository;
-import com.streaming.fb_streamingplatform.service.StreamingService;
 import javafx.application.Application;
-
-import java.sql.SQLException;
 
 public class Launcher {
 
-    public static void main(String[] args) throws SQLException {
-        // Application.launch(MainApp.class, args);
-        StreamingService streamingService = new StreamingService();
-        streamingService.addFavorite(5, 5);
+    public static void main(String[] args) {
+        Application.launch(MainApp.class, args);
+
     }
 }
